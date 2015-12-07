94823b0d8edb48a092536b03edcc40bbdf1924b2
Selen ADALI <selenadali94@gmail.com>
2015-12-07 23:09:31 +0200

    a

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace damacanadb.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+    }
+    public class ProductDBContext : DbContext
+    {
+        public DbSet<Product> products { get; set; }
+    }
+}
